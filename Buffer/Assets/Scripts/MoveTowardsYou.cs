91bb567c55b14f9939eb464149722a8afefebffa
Ciaran <ciaran2293@yahoo.con>
2018-01-26 23:08:08 +0000

    Created MoveTowardsYou class, added player tag and enemy tag

---
+++ Buffer/Assets/Scripts/MoveTowardsYou.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardsYou : MonoBehaviour {
+
+    // Use this for initialization
+    public float currentYPos;
+    public float endYpos= -5f;
+    public float speed=0.01f;
+	void Start () {
+        currentYPos = this.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Vector3.down * Time.deltaTime*speed;
+        currentYPos = this.transform.position.y;
+        CheckY();
+    }
+    //Destroys The object onces it pasts out of the game
+    public void CheckY()
+    {
+        if (currentYPos < endYpos)
+            Destroy(gameObject);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            Destroy(gameObject);
+    }
+}
