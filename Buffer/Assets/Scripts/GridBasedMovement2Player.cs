1b0a5dd28dff7709b0bf25c7a4978670493064fd
Daniel Boyle <danielj93@gmail.com>
2018-01-26 15:43:43 -0800

    Added player movement script

--- Buffer/Assets/Scripts/GridBasedMovement2Player.cs
+++ Buffer/Assets/Scripts/GridBasedMovement2Player.cs
@@ -3,62 +3,55 @@ using System.Collections;
 
 public class GridBasedMovement2Player : MonoBehaviour {
 
-	float speed = 2.0f;
-	Vector3 pos;
+	float speed = 50.0f;
+    bool changepos;
+    Vector3 pos;
 	Vector3 oldpos;
+    Vector3 maxLeftPos;
+    Vector3 maxRightPos;
 	Transform tr;
-	bool changepos;
-	public AudioClip Bump;
 
 	// Use this for initialization
 	void Start () 
 	{
-		pos = transform.position;
+        maxLeftPos = new Vector3(-5, 0, 0);
+        maxRightPos = new Vector3(5, 0, 0);
+        pos = transform.position;
 		tr = transform;
 		oldpos = transform.position;
 	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		if (Input.GetKey(KeyCode.RightArrow) && tr.position == pos)
-		{
-			pos += Vector3.right;
-			oldpos = transform.position;
 
+    // Update is called once per frame
+    void Update() {
+        PlayerMovement();
+    }
+   
+    void PlayerMovement() {
 
-		}
-		else if (Input.GetKey(KeyCode.LeftArrow) && tr.position == pos)
-		{
-			pos += Vector3.left;
-			oldpos = transform.position;
+        if (pos.x != maxRightPos.x) {
+            if (Input.GetKey(KeyCode.RightArrow) && tr.position == pos) {
+                Debug.Log(pos);
+                pos += new Vector3(5, 0, 0);
+                oldpos = transform.position;
+            }
 
-		}
-		else if (Input.GetKey(KeyCode.UpArrow) && tr.position == pos)
-		{
-			pos += Vector3.up;
-			oldpos = transform.position;
+        }
 
-		}
-		else if (Input.GetKey(KeyCode.DownArrow) && tr.position == pos)
-		{
-			pos += Vector3.down;
-			oldpos = transform.position;
+        if (pos.x != maxLeftPos.x) {
+            if (Input.GetKey(KeyCode.LeftArrow) && tr.position == pos) {
+                pos += new Vector3(-5, 0, 0);
+                oldpos = transform.position;
 
-		}
+            }
+        }
 
-		if (changepos) {
-			pos = oldpos;
-			changepos = false;
-		}
-
-
-		transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+        if (changepos) {
+            pos = oldpos;
+            changepos = false;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+    }
+		
 	}
-	void OnCollisionEnter()
-	{
-		GetComponent<AudioSource>().Play ();
-		changepos = true;
-		//Debug.Log ("YAY");
-	}
-}
+    
+
