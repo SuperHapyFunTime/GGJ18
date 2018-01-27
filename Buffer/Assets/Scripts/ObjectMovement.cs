7b1dd5a691e66c9454a9859460a7c02ff6eb549c
Daniel Boyle <danielj93@gmail.com>
2018-01-27 07:05:42 -0800

    Added slider and enemy momement

---
+++ Buffer/Assets/Scripts/ObjectMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour {
+
+    // Use this for initialization
+    public float currentXPos;
+    public float endYpos= -5f;
+    public float speed=0.01f;
+	void Start () {
+        currentXPos = this.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Vector3.left * Time.deltaTime*speed;
+        currentXPos = this.transform.position.x;
+        CheckY();
+    }
+    //Destroys The object onces it pasts out of the game
+    public void CheckY()
+    {
+        if (currentXPos < endYpos)
+            Destroy(gameObject);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            Destroy(gameObject);
+    }
+}
---
+++ Buffer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	float lerpSpeed;
+    float playerJumpDist;
+    Vector3 playerCurrentPos;
+	Vector3 playerPreviousPos;
+    Vector3 maxLeftPos;
+    Vector3 maxRightPos;
+	Transform tr;
+
+	// Use this for initialization
+	void Start () 
+	{
+        maxLeftPos = new Vector3(0, -5, 0);
+        maxRightPos = new Vector3(0, 5, 0);
+        lerpSpeed = 50.0f;
+        playerJumpDist = 5;
+        playerCurrentPos = transform.position;
+		tr = transform;
+		playerPreviousPos = transform.position;
+	}
+
+    // Update is called once per frame
+    void Update() {
+        playerMovement();
+    }
+   
+    void playerMovement() {
+
+        if (playerCurrentPos.y != maxRightPos.y) {
+            if (Input.GetKeyDown(KeyCode.UpArrow) && tr.position == playerCurrentPos) {
+                playerCurrentPos += new Vector3(0, 5, 0);
+                playerPreviousPos = transform.position;
+            }
+
+        }
+
+        if (playerCurrentPos.y != maxLeftPos.y) {
+            if (Input.GetKeyDown(KeyCode.DownArrow) && tr.position == playerCurrentPos) {
+                playerCurrentPos += new Vector3(0, -5, 0);
+                playerPreviousPos = transform.position;
+
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, playerCurrentPos, Time.deltaTime * lerpSpeed);
+    }
+		
+	}
+    
+
